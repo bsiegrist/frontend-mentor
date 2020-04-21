8b89425c9e2c517f5cac49016b5bd0b6d794ddaa
Barbara Siegrist <siegrist.barbara@gmail.com>
2020-04-21 16:09:40 +0200

    write score, add function to rules-button

--- app/js/app.js
+++ app/js/app.js
@@ -1,2 +1,19 @@
 const rulesbutton = document.querySelector(".rulesbutton");
+const rules = document.querySelector(".rules");
+const score = document.querySelector(".score__points");
 
+let points = 0;
+
+
+//show rules
+rulesbutton.addEventListener('click', function(){
+    rules.style.display = "flex";
+});
+
+//hide rules
+rules.addEventListener('click', function(){
+    rules.style.display = "none";
+})
+
+//show points
+score.innerText = points;
